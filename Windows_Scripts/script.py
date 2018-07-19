55484cbd8a61d29e35311a2f76232912dbcaa470
mameeks <38370081+mameeks@users.noreply.github.com>
2018-07-19 14:13:37 +0800

    Delete script.py

--- Windows_Scripts/script.py
+++
@@ -1,9 +0,0 @@
-# from TdmsToCSV import TdmsToCSV
-from LoadData import LoadData
-# from SaveTriggers import SaveTriggers
-# from PlotTriggers import PlotTriggers
-
-# TdmsToCSV()
-LoadData('/Users/malika/Documents/MATLAB/behavior/Nacre WT_7day_1per_chronic_alcohol_exp')
-# SaveTriggers()
-# PlotTriggers()
