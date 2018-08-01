8bd98c672474e2351aaf83153800d38e02a01d40
mameeks <38370081+mameeks@users.noreply.github.com>
2018-08-01 10:43:31 +0800

    Add files via upload

--- Windows_Scripts/SaveTriggers.py
+++ Windows_Scripts/SaveTriggers.py
@@ -25,7 +25,7 @@ def SaveTriggers(Tmin, Tmax, Bin_Lengths):
     # data_folder = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please select folder containing fish data:')
     # data_folder = '%s/FishData' % data_folder
     data_folder = raw_input("Input name of folder containing LoadData output: ")
-    data_folder = '/Users/malika/Documents/MATLAB/behavior/%s/FishData' % data_folder
+    data_folder = '/Users/malika/Documents/IMCB/Behavior/%s/FishData' % data_folder
 
     # Define function to sort .csv files in order
     _nsre = re.compile('([0-9]+)')
@@ -35,7 +35,7 @@ def SaveTriggers(Tmin, Tmax, Bin_Lengths):
 
     # Define function to sort bins
     def binstore(File, trigger, df, i, ROI):
-
+        trigger_time = len(np.nonzero(trigger)[0]) * 1/(File['Sampling_Rate'][1])
         # Save only consecutive 1s in trigger
         trigger = trigger.tolist()
         trigger = ''.join([str(x) for x in trigger])
@@ -45,10 +45,10 @@ def SaveTriggers(Tmin, Tmax, Bin_Lengths):
         if not num_consec:
             result = [0] * len(Start)
         else:
-            # length_num_consec = []
+            length_num_consec = []
             length_num_consec_ms = []
             for k, num in enumerate(num_consec):
-                # length_num_consec.append(len(num))
+                length_num_consec.append(len(num))
                 length_num_consec_ms.append(len(num)*(1/File['Sampling_Rate'][1])*1000)
 
             # Sort into bins
@@ -57,7 +57,7 @@ def SaveTriggers(Tmin, Tmax, Bin_Lengths):
             result = np.delete(result, 0)
             result = result.astype('S15')
             result = np.insert(result, 0, "Fish %s %s" % (i, ROI))
-            result = np.append(result, len(np.nonzero(trigger)[0]) * 1/(File['Sampling_Rate'][1]))
+            result = np.append(result, trigger_time)
             df.loc[len(df)] = result
         return result
 
