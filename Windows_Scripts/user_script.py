130362628c08da3fb38d30ba90e5123afb087cc2
mameeks <38370081+mameeks@users.noreply.github.com>
2018-12-11 10:50:24 +0800

    Update user_script.py

--- Windows_Scripts/user_script.py
+++ Windows_Scripts/user_script.py
@@ -13,6 +13,7 @@ from Sparklines_full import Sparklines_full
 # PlotTriggers()
 # SaveTriggers(TMin=0, TMax=1000, Bin_Lengths=['0-1000', '1000-2000', '2000-3000', '3000-5000','5000-8000',
 #                    '8000-11000', '11000-15000', '15000-20000'])
+# DescribeTriggers()
 #
 # ROIData(Coordinates = np.array([ [0,50], [0,75], [15,50], [15,75] ]), TMin = 0, TMax = 180)
 # ROIData(Coordinates = np.array([ [15,50], [15,75], [30,50], [30,75] ]), TMin = 0, TMax = 180)
